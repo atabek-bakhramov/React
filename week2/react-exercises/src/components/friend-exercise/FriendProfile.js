d6a67d0675c021a2c2e12fe482e182d2f5cad7bc
atabek-bakh <coderonline@icloud.com>
2021-03-29 13:34:33 +0200

    updated

--- week2/react-exercises/src/components/friend-exercise/FriendProfile.js
+++ week2/react-exercises/src/components/friend-exercise/FriendProfile.js
@@ -1,19 +1,25 @@
 const FriendProfile = ({ data }) => {
-  const { first, last }  = data.name;
-  const { number, name } = data.location.street; 
+  const { first, last } = data.name;
+  const { number, name } = data.location.street;
   const { city, country } = data.location;
   const { email, cell } = data;
 
   return (
-    <div>
-      <ul>
-        <li>Your new friend's name: {first} {last}</li>
-        <li>The friend is from: {city}, {country} </li>
-        <li>The address: {number} {name}</li>
-        <li>You can connect to the friend via: {email} or {cell}</li>
-      </ul>
-    </div>
-  )
+    <ul>
+      <li>
+        Your new friend's name: {first} {last}
+      </li>
+      <li>
+        The friend is from: {city}, {country}{" "}
+      </li>
+      <li>
+        The address: {number} {name}
+      </li>
+      <li>
+        You can connect to the friend via: {email} or {cell}
+      </li>
+    </ul>
+  );
 };
 
 export default FriendProfile;
